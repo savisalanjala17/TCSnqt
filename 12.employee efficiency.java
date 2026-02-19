0b487a653e8497e192d1ab705536ff4dbda96dae
savisalanjala17 <savisalanjala17@gmail.com>
2026-02-19 12:14:40 +0530

    Create 12.employee efficiency.java

---
+++ 12.employee efficiency.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       
+       int a[] = new int [n];
+       
+       for(int i=0 ; i<n ;i++){
+           a[i]=sc.nextInt();
+       }
+       
+       Arrays.sort(a);
+       
+       int total1=a[n-1]*a[n-2]*a[n-3];
+       int total2=a[0]*a[1]*a[n-1];
+       
+       int result = total1>total2?total1:total2;
+       
+       System.out.print(result);
+       
+   
+   }
+}
