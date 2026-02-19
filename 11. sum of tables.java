4025b70ed094ed92998fe6d6491c1e9a501f2c3f
savisalanjala17 <savisalanjala17@gmail.com>
2026-02-19 12:02:19 +0530

    Add sum of tables program in Java
    Implement a program to calculate the sum of the first 10 multiples of a given number.

---
+++ 11. sum of tables.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       
+       int total = 10*(10+1)/2;
+       System.out.print(n*total);
+       
+   
+   }
+}
